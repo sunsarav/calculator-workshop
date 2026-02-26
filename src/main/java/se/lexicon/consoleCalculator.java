197967215bf4aabdc810d700a814962defaf0893
Shanmuga Saravanan <sunsarav16@gmail.com>
2026-02-26 16:01:33 +0100

    project completed

--- src/main/java/se/lexicon/consoleCalculator.java
+++ src/main/java/se/lexicon/consoleCalculator.java
@@ -3,58 +3,65 @@ package se.lexicon;
 import java.util.Scanner;
 
 public class consoleCalculator {
-    static void main() {
+    static void main(String[] args) {
 
         {
             Scanner scanner = new Scanner(System.in);
             boolean running = true;
-            String choice;
 
             do {
-                System.out.println("Enter first number: ");
+                System.out.print("Enter first number: ");
                 double num1 = scanner.nextDouble();
 
-                System.out.println("Enter the second number: ");
+                System.out.print("Enter the second number: ");
                 double num2 = scanner.nextDouble();
 
-                System.out.println("Enter the operator(+,-,*,/): ");
-                char operator = scanner.next().charAt(0);
+                boolean validOp = false;
+                while (!validOp) {
+                    System.out.print("Enter the operator(+,-,*,/): ");
+                    char operator = scanner.next().charAt(0);
 
-                double add = num1 + num2;
-                double sub = num1 - num2;
-                double mul = num1 * num2;
-                double div = num1 / num2;
+                    switch (operator) {
 
-                switch (operator) {
+                        case '+':
+                            System.out.println("Result: " + (num1 + num2));
+                            validOp = true;
+                            break;
+                        case '-':
+                            System.out.println("Result: " + (num1 - num2));
+                            validOp = true;
+                            break;
+                        case '*':
+                            System.out.println("Result: " + (num1 * num2));
+                            validOp = true;
+                            break;
+                        case '/':
+                            if (num2 != 0) {
+                                System.out.println("Result: " + (num1 / num2));
+                                validOp = true;
+                            } else {
+                                System.out.println("Error: Cannot divide by zero!");
+                                validOp = true;
+                            }
+                            break;
+                        default:
+                            System.out.println("Invalid input! Please select proper operator");
 
-                    case '+':
-                        System.out.println(add);
-                        break;
-                    case '-':
-                        System.out.println(sub);
-                        break;
-                    case '*':
-                        System.out.println(mul);
-                        break;
-                    case '/':
-                        System.out.println(div);
-                        break;
-                    default:
-                        System.out.println("Please select proper operator(+,-,*,/): ");
-                        break;
+                    }
                 }
-
                 System.out.println("Do you want to perform another calculation (y/n): ");
-                choice = scanner.next();
+                String choice = scanner.next();
 
                 if (choice.equalsIgnoreCase("n")) {
                     running = false;
                 }
             } while (running);
             System.out.println("Program exited");
+
         }
     }
 }
+
 
 
 
